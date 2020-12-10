de5812ce802ce4d8b645463ebae6a895165e1b9e
dpetrocelli <soporte@localhost.localdomain>
2020-12-10 19:51:24 -0300

    basic test 2

--- test/entrypoint.sh
+++ test/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 sh -c "echo Entry point..."
-sh -c "echo USERNAME: $username"
-sh -c "echo PASSWORD: $password"
-sh -c "echo VCENTER: $vcenterhost"
+sh -c "echo USERNAME: $INPUT_USERNAME"
+sh -c "echo PASSWORD: $INPUT_PASSWORD"
+sh -c "echo VCENTER: $INPUT_VCENTER"
 
 #sh -c "ab -n $INPUT_NUMBEROFREQUESTS -c $INPUT_CONCURRENCY $INPUT_URL"
